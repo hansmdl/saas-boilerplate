f10c2865e0c1177c46bdb5338cf7f92e64a6ae8c
Hans Minyetty <hansmdleon@gmail.com>
2025-07-02 23:10:45 -0400

    chore: untrack dist directory

--- apps/api/dist/organization/dto/create-organization.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.createOrganizationSchema = void 0;
-const zod_1 = require("zod");
-exports.createOrganizationSchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, 'Organization name is required'),
-});
